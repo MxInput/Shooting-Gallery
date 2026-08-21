a8e396c43d8dde21a43f2f36f18a4c214db81956
MxInput <mxinput@protonmail.com>
2026-08-20 23:31:42 -0400

    equip button move effect

--- scripts/cross_hair_container.gd
+++ scripts/cross_hair_container.gd
@@ -3,6 +3,12 @@ extends TextureRect
 var selected_item : ShopItem;
 var unlocked := false;
 
+@export var equip_button : TextureButton;
+
 func _on_equip_button_down() -> void:
-	PlayerVariables.cross_hair_texture = selected_item.item_texture;
-	PlayerVariables.write_to_save();
+	if (unlocked):
+		PlayerVariables.cross_hair_texture = selected_item.item_texture;
+		PlayerVariables.write_to_save();
+	else:
+		if (!equip_button.is_moving()):
+			equip_button.start_moving();
--- scripts/item_color_change.gd
+++ scripts/item_color_change.gd
@@ -12,6 +12,12 @@ extends TextureButton
 
 @export var red_color : Color;
 
+var original_pos : Vector2;
+var time := 0.0;
+var moving := false;
+var moving_right := true;
+var distance := 2.0;
+
 func _on_mouse_entered() -> void:
 	label.add_theme_color_override("font_color", new_color);
 
@@ -21,7 +27,44 @@ func _on_mouse_exited() -> void:
 	else:
 		label.add_theme_color_override("font_color", red_color);
 
+func start_moving() -> void:
+	moving = true;
+	moving_right = true;
+	original_pos = position;
 
+func jiggle() -> void:
+	if (moving_right):
+		position.x += 0.4;
+		
+		var end_pos = original_pos.x + distance;
+		
+		if (position.x >= end_pos):
+			moving_right = false;
+	else:
+		position.x -= 0.4;
+		
+		var end_pos = original_pos.x - distance;
+		
+		if (position.x <= end_pos):
+			moving_right = true;
+		
+func is_moving() -> bool:
+	return moving;
+	
+func _process(delta: float) -> void:
+	if (moving):
+		time += delta;
+		
+		jiggle();
+	
+		if (time > 1.0):
+			moving = false;
+			moving_right = true;
+			
+			position = original_pos;
+			
+			time = 0.0;
+			
 func _on_crosshair_container_renamed() -> void:
 	if (!container.unlocked):
 		texture_normal = red_button_normal;
